934285d1832b6a863ac01dc901fce0dceda0c6c4
yunchu <yunchu@live.unc.edu>
2016-10-29 18:09:53 -0400

    plz work

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Test from './Test.jsx';
+
+class App extends React.Component {
+   render() {
+      return (
+         <Test>
+            Hello World!!!!!!
+         </Test>
+      );
+   }
+}
+
+export default App;
